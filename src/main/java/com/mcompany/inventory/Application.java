20636bc494558ca32b00aaa4b2a8f35f94bb6a7d
Vanilson <vanilson.m.santos@gmail.com>
2015-09-13 22:37:05 -0300

    Commented the script to populate database

--- src/main/java/com/mcompany/inventory/Application.java
+++ src/main/java/com/mcompany/inventory/Application.java
@@ -2,15 +2,13 @@ package com.mcompany.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext context = new FileSystemXmlApplicationContext("/migrations/dao.xml");
-        context.getBean("mongeez");
+//        ApplicationContext context = new FileSystemXmlApplicationContext("/migrations/dao.xml");
+//        context.getBean("mongeez");
         SpringApplication.run(Application.class, args);
     }
 }

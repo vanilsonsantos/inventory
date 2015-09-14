2bb430fda92bb9432d5cb6760895b6ac8baf18dd
Vanilson <vanilson.m.santos@gmail.com>
2015-09-14 12:52:46 -0300

    Added application properties v2

--- src/main/java/com/mcompany/inventory/Application.java
+++ src/main/java/com/mcompany/inventory/Application.java
@@ -7,13 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-//        ApplicationContext context = new FileSystemXmlApplicationContext("/migrations/dao.xml");
-//        context.getBean("mongeez");
-//        String webPort = System.getenv("PORT");
-//        if (webPort == null || webPort.isEmpty()) {
-//            webPort = "8080";
-//        }
-//        System.setProperty("server.port", webPort);
         SpringApplication.run(Application.class, args);
     }
 }
